2af58a4107763d0348c7afb2fa2532eede34423a
SUDHEER341 <sudheer601220@gmail.com>
2023-02-11 13:34:19 +0530

    Added : Display board

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,7 +6,6 @@ public class TicTacToe {
     static char[] board = new char[10];
     static char player, computer;
     Scanner scanner = new Scanner(System.in);
-
     public void initialize() {
         for (i = 1; i < 10; i++) {
             board[i] = ' ';
@@ -18,29 +17,27 @@ public class TicTacToe {
         if (choice == 'X') {
             player = 'X';
             computer = 'O';
-            System.out.println("player = " + player );
-            System.out.println("computer = " + computer );
-
-        } else if (choice == '0') {
+        } else if (choice == 'O') {
             player = 'O';
             computer = 'X';
-            System.out.println("player = " + player );
-            System.out.println("computer = " + computer );
         }
-
     }
+    public void showboard() {
 
-
-
-    public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe");
         System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
-        TicTacToe obj1 =new TicTacToe();
-        obj1.check();
+
     }
 
+    public static void main(String[] args) {
+
+        TicTacToe object = new TicTacToe();
+        object.initialize();
+        object.showboard();
+
+    }
 }
